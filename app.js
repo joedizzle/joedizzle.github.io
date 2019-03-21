d0300e93c943fbe0233a8e8d25aa6ae97b0ffa3e
Joe Mwangi <joedizzle36@yahoo.com>
2019-03-21 16:52:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-
+<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBjfi_HGpuxInEHEQzgSLPEW3Be9VdjaPo",
+    authDomain: "default-3ca12.firebaseapp.com",
+    databaseURL: "https://default-3ca12.firebaseio.com",
+    projectId: "default-3ca12",
+    storageBucket: "default-3ca12.appspot.com",
+    messagingSenderId: "840670412769"
+  };
+  firebase.initializeApp(config);
+</script>
